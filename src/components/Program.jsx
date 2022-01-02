cd2ea6247c4df1ae269013205441e24c3c24e280
Johan Ehinger <johan.ehinger@hotmail.com>
2022-01-02 19:32:46 +0100

    refactor: Add new colors and styling to program cards

--- src/components/Program.jsx
+++ src/components/Program.jsx
@@ -1,16 +1,15 @@
 import * as React from "react";
-import {
-  Card,
-  CardContent,
-  Typography,
-  CardActions,
-  CardMedia,
-  Button,
-} from "@mui/material";
+import { Card, CardContent, Typography, CardMedia } from "@mui/material";
 
 function Program(props) {
   return (
-    <Card style={{ height: "550px", backgroundColor: props.item.color }}>
+    <Card
+      style={{
+        height: "550px",
+        backgroundColor: props.item.color,
+        borderRadius: 20,
+      }}
+    >
       <CardMedia component="img" image={props.item.image} />
       <CardContent>
         <Typography align="center" variant="h4">
@@ -18,10 +17,6 @@ function Program(props) {
         </Typography>
         <Typography variant="body1">{props.item.description}</Typography>
       </CardContent>
-
-      <CardActions>
-        <Button>Check it out!</Button>
-      </CardActions>
     </Card>
   );
 }
