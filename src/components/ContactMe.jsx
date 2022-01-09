05f114905cc8e7fb4918d336025ad9aeab0291fe
Johan Ehinger <johan.ehinger@hotmail.com>
2022-01-09 08:04:01 +0100

    refactor: Redo design in transformation and contact me sections

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,8 +1,61 @@
 import * as React from "react";
-import { TextField, Container, Typography, Button, Grid } from "@mui/material";
+import {
+  TextField,
+  Typography,
+  Button,
+  Grid,
+  Box,
+  Card,
+  CardContent,
+} from "@mui/material";
 import Spacer from "./Spacer";
 
 function ContactMe() {
+  const image = process.env.PUBLIC_URL + "/backgroundPicture2.png";
+  return (
+    <Box name="contact-me-section">
+      <Box
+        sx={{ display: { xs: "none", md: "flex" } }}
+        style={{
+          flexGrow: 1,
+          minHeight: "650px",
+          backgroundColor: "#c3ccce",
+          backgroundImage: `url(${image})`,
+          backgroundRepeat: "no-repeat",
+          backgroundAttachment: "fixed",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          alignItems: "center",
+        }}
+      >
+        <Grid container columns={12} alignItems="center">
+          <Grid item md={1}></Grid>
+          <Grid item xs={12} md={5}>
+            <ContactMeCard />
+          </Grid>
+        </Grid>
+      </Box>
+      <Box
+        sx={{ display: { xs: "flex", md: "none" } }}
+        style={{
+          flexGrow: 1,
+          minHeight: "650px",
+
+          alignItems: "center",
+        }}
+      >
+        <Grid container columns={12} alignItems="center">
+          <Grid item md={1}></Grid>
+          <Grid item xs={12} md={5}>
+            <ContactMeCard />
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+}
+
+function ContactMeCard() {
   const [name, setName] = React.useState("");
   const [phoneNumber, setphoneNumber] = React.useState("");
   const [email, setEmail] = React.useState("");
@@ -32,110 +85,112 @@ function ContactMe() {
     console.log(email);
     console.log(message);
   };
-
   return (
-    <Container name="contact-me-section" maxWidth="sm" sx={{ py: 2 }}>
-      <Typography
-        align="center"
-        variant="h4"
-        sx={{ display: { xs: "none", md: "block" }, py: 1 }}
-      >
-        Kontakta mig
-      </Typography>
-      <Typography
-        align="center"
-        variant="h5"
-        sx={{ display: { xs: "block", md: "none" } }}
-      >
-        Kontakta mig
-      </Typography>
-      <Spacer />
-      <form onSubmit={handleSubmit}>
-        <Grid container columns={12} spacing={2}>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            style={{
-              alignSelf: "center",
-            }}
-          >
-            <TextField
-              fullWidth
-              label="Namn"
-              value={name}
-              onChange={handleNameChange}
-              color="text"
-            />
-          </Grid>
+    <Card elevation={0}>
+      <CardContent>
+        <Typography
+          align="center"
+          variant="h4"
+          sx={{ display: { xs: "none", md: "block" }, py: 1 }}
+        >
+          Kontakta mig
+        </Typography>
+        <Typography
+          align="center"
+          variant="h5"
+          sx={{ display: { xs: "block", md: "none" } }}
+        >
+          Kontakta mig
+        </Typography>
 
-          <Grid
-            item
-            xs={12}
-            md={6}
-            style={{
-              alignSelf: "center",
-            }}
-          >
-            <TextField
-              fullWidth
-              label="Telefonnummer"
-              value={phoneNumber}
-              onChange={handlePhoneNumberChange}
-              color="text"
-            />
-          </Grid>
+        <Spacer />
+        <form onSubmit={handleSubmit}>
+          <Grid container columns={12} spacing={2}>
+            <Grid
+              item
+              xs={12}
+              md={6}
+              style={{
+                alignSelf: "center",
+              }}
+            >
+              <TextField
+                fullWidth
+                label="Namn"
+                value={name}
+                onChange={handleNameChange}
+                color="text"
+              />
+            </Grid>
 
-          <Grid
-            item
-            xs={12}
-            md={6}
-            style={{
-              alignSelf: "center",
-            }}
-          >
-            <TextField
-              fullWidth
-              label="E-post"
-              value={email}
-              onChange={handleEmailChange}
-              color="text"
-            />
+            <Grid
+              item
+              xs={12}
+              md={6}
+              style={{
+                alignSelf: "center",
+              }}
+            >
+              <TextField
+                fullWidth
+                label="Telefonnummer"
+                value={phoneNumber}
+                onChange={handlePhoneNumberChange}
+                color="text"
+              />
+            </Grid>
+
+            <Grid
+              item
+              xs={12}
+              md={6}
+              style={{
+                alignSelf: "center",
+              }}
+            >
+              <TextField
+                fullWidth
+                label="E-post"
+                value={email}
+                onChange={handleEmailChange}
+                color="text"
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              md={12}
+              style={{
+                alignSelf: "center",
+              }}
+            >
+              <TextField
+                fullWidth
+                multiline
+                minRows={3}
+                maxRows={5}
+                label="Meddelande"
+                value={message}
+                onChange={handleMessageChange}
+                color="text"
+              />
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              md={12}
+              style={{
+                alignSelf: "center",
+              }}
+            >
+              <Button type="submit" size="large" variant="contained">
+                Skicka
+              </Button>
+            </Grid>
           </Grid>
-          <Grid
-            item
-            xs={12}
-            md={12}
-            style={{
-              alignSelf: "center",
-            }}
-          >
-            <TextField
-              fullWidth
-              multiline
-              minRows={3}
-              maxRows={5}
-              label="Meddelande"
-              value={message}
-              onChange={handleMessageChange}
-              color="text"
-            />
-          </Grid>
-          <Grid
-            item
-            xs={12}
-            md={12}
-            style={{
-              alignSelf: "center",
-            }}
-          >
-            <Button type="submit" size="large" variant="contained">
-              Skicka
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
-    </Container>
+        </form>
+      </CardContent>
+    </Card>
   );
 }
 
