344d1bdbd4ed06719165f0ab6c853590dc4c38c2
Jesper <jesperjen98@gmail.com>
2022-01-18 17:54:26 +0100

    Appbar now looks good on mobile

--- src/components/PageAppBar.jsx
+++ src/components/PageAppBar.jsx
@@ -42,24 +42,59 @@ function PageAppBar() {
     });
   };
   return (
-    <AppBar position="absolute" color="transparent" elevation={0}>
-      <Toolbar>
-        <Box
-          width={1}
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-            alignItems: "center",
-          }}
-        >
-          <Box width={1 / 5}></Box>
-          <Box width={1 / 5}>
-            <Typography align="center" marginTop={2} variant="h3">
-              Hanna Ljungsten
-            </Typography>
-          </Box>
-          <Box width={1 / 5} sx={{ display: { xs: "none", md: "flex" } }}>
+    <Box>
+      {/* Phone */}
+      <AppBar
+        position="absolute"
+        color="transparent"
+        elevation="0"
+        sx={{
+          display: { xs: "flex", md: "none" },
+          padding: "10px",
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            size="large"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+            sx={{ flexGrow: 1, justifyContent: "start", padding: "0" }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            align="center"
+            variant="h5"
+            sx={{
+              width: "170px",
+              lineHeight: "1",
+              letterSpacing: "2px",
+              fontWeight: "bold",
+            }}
+          >
+            HANNA LJUNGSTEN
+          </Typography>
+          <Box sx={{ flexGrow: 1 }}></Box>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: "block", md: "none" },
+            }}
+          >
             <Button
               variant="text"
               color="textColor"
@@ -74,36 +109,34 @@ function PageAppBar() {
             >
               Kontakta Mig
             </Button>
-          </Box>
-          <Box width={1 / 5} sx={{ display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
+          </Menu>
+        </Toolbar>
+        <AppBar></AppBar>
+      </AppBar>
+      {/* Website */}
+      <AppBar
+        position="absolute"
+        color="transparent"
+        elevation={0}
+        sx={{ display: { xs: "none", md: "flex" } }}
+      >
+        <Toolbar>
+          <Box
+            width={1}
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+            }}
+          >
+            <Box width={1 / 5}></Box>
+            <Box width={1 / 5}>
+              <Typography align="center" marginTop={2} variant="h3">
+                Hanna Ljungsten
+              </Typography>
+            </Box>
+            <Box width={1 / 5}>
               <Button
                 variant="text"
                 color="textColor"
@@ -118,11 +151,54 @@ function PageAppBar() {
               >
                 Kontakta Mig
               </Button>
-            </Menu>
+            </Box>
+            <Box>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+              >
+                <Button
+                  variant="text"
+                  color="textColor"
+                  onClick={handleHowItWorksClick}
+                >
+                  Så Funkar Det
+                </Button>
+                <Button
+                  variant="text"
+                  color="textColor"
+                  onClick={handleContactMeClick}
+                >
+                  Kontakta Mig
+                </Button>
+              </Menu>
+            </Box>
           </Box>
-        </Box>
-      </Toolbar>
-    </AppBar>
+        </Toolbar>
+        <AppBar></AppBar>
+      </AppBar>
+    </Box>
   );
 }
 
