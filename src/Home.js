6f030eb7e2ee4f87220ef22ab62773a256146581
Johan Ehinger <johan.ehinger@hotmail.com>
2022-03-22 20:03:30 +0100

    Fix design on programs

--- src/Home.js
+++ src/Home.js
@@ -1,41 +1,37 @@
-import * as React from "react";
-import PageAppBar from "./components/PageAppBar";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import AboutMe from "./components/AboutMe";
-import SocialMedia from "./components/SocialMedia";
-import Spacer from "./components/Spacer";
-import ProgramDescriptions from "./components/ProgramDescriptions";
-import TheProcess from "./components/TheProcess";
-import MyStory from "./components/MyStory";
-import ContactMe from "./components/ContactMe";
-import ProgramMiscellaneous from "./components/ProgramMiscellaneous";
-import PageFooter from "./components/PageFooter";
-import FAQ from "./components/FAQ";
-import Transformations from "./components/Transformations";
+import * as React from 'react'
+import PageAppBar from './components/PageAppBar'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import AboutMe from './components/AboutMe'
+import Spacer from './components/Spacer'
+import ProgramDescriptions from './components/ProgramDescriptions'
+import MyStory from './components/MyStory'
+import ContactMe from './components/ContactMe'
+import ProgramMiscellaneous from './components/ProgramMiscellaneous'
+import PageFooter from './components/PageFooter'
 
 const theme = createTheme({
   typography: {
-    fontFamily: ["Ledger", "serif"].join(","),
+    fontFamily: ['Ledger', 'serif'].join(',')
   },
   palette: {
     primary: {
       // main: "#6cc5d5",
-      main: "#eac6b7",
-      contrastText: "#FFFFFF",
+      main: '#eac6b7',
+      contrastText: '#FFFFFF'
     },
     secondary: {
-      light: "#0066ff",
-      main: "#0044ff",
-      contrastText: "#ffcc00",
+      light: '#0066ff',
+      main: '#0044ff',
+      contrastText: '#ffcc00'
     },
     white: {
-      main: "#ffffff",
+      main: '#ffffff'
     },
     textColor: {
-      main: "#000000",
-    },
-  },
-});
+      main: '#000000'
+    }
+  }
+})
 
 function Home() {
   return (
@@ -43,25 +39,16 @@ function Home() {
       <React.Fragment>
         <PageAppBar />
         <AboutMe />
-        {/* <Spacer /> */}
-        {/* <TheProcess /> */}
-        <Spacer />
         <ProgramDescriptions />
         <Spacer />
         <ProgramMiscellaneous />
         <Spacer />
         <ContactMe />
-        {/* <Spacer /> */}
-        {/* <Transformations /> */}
-        {/* <Spacer /> */}
         <MyStory />
-        {/* <SocialMedia /> */}
-        {/* <Spacer /> */}
-        {/* <FAQ /> */}
         <PageFooter />
       </React.Fragment>
     </ThemeProvider>
-  );
+  )
 }
 
-export default Home;
+export default Home
